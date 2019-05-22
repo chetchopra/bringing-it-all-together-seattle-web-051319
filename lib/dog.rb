9ff7ae5f53a5b2c5011054ff82f1f4255afb3368
Chet  Chopra <chopra.chet@gmail.com>
2019-05-22 21:33:31 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,35 @@
+class Dog
+  
+  attr_accessor :name, :breed, :id
+  
+  def initialize(hash)
+    @name = hash[:name]
+    @breed = hash[:breed]
+    @id = hash[:id]
+  end
+  
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        );
+        SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE dogs;
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def save 
+    
+  end
+      
+
+end
