aebe2279278454c685ac0d382662d5b81dacf4ab
Chet  Chopra <chopra.chet@gmail.com>
2019-05-22 22:02:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -60,7 +60,7 @@ class Dog
       WHERE id = ?
     SQL
     result = DB[:conn].execute(sql, id)[0]
-    Dog.new(result[0], result[1], result[2])
+    Dog.new({:id => result[0], :name => result[1], :breed => result[2]})
   end
   
   # def self.find_or_create_by(hash)
