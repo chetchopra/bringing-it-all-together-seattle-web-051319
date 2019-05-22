2d393823897268f339c956bf1b8a217d5f500400
Chet  Chopra <chopra.chet@gmail.com>
2019-05-22 22:06:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -76,7 +76,7 @@ class Dog
   
   def self.find_by_name(name)
     result = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?", name)[0]
-    
+    return Dog.new({:id => result[0], :name => result[1], :breed => result[2]})
   end
     
     
