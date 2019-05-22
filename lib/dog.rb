af1fa0a6ea5886b4ba5da24e7dc269d0fd4de4fe
Chet  Chopra <chopra.chet@gmail.com>
2019-05-22 21:47:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -63,7 +63,7 @@ class Dog
   
   def self.find_or_create_by(hash)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? and breed = ?", hash[:name], hash[:breed])
-    
+    if !dog.empty?
   end 
     
     
